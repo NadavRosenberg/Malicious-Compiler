2c43507b60faf2029eb46a862eca81dc9c94c9ab
Nadav <rosenbergnadav@gmail.com>
2021-06-07 16:16:55 +0300

    fix

---
+++ TheCompiler.java
@@ -0,0 +1,9 @@
+// nonce
+
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class TheCompiler {
+  public static void main(String[] args) {
+    showMessageDialog(null, "hello world!");
+  }
+}
