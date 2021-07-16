29e40a512a2022bfcbedc3cb9079a9cfd27ef8f6
Shlomi Haver <shlomi.haver@booking.com>
2021-07-16 12:50:02 +0300

    adding example file

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        showMessageDialog(null, "hello world!");
+        System.out.println("Program finished");
+    }
+}
