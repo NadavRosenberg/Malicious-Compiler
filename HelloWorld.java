2766eb44e756b302bb6d83bd45f605e533a96090
Shlomi Haver <shlomi.haver@booking.com>
2021-07-17 14:37:30 +0300

    adding more prints

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ import static javax.swing.JOptionPane.showMessageDialog;
 
 public class HelloWorld {
     public static void main(String[] args) {
-
+        System.out.println("Program started");
         showMessageDialog(null, "hello world!");
         System.out.println("Program finished");
     }
