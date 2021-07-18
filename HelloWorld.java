c70c247c6b34fe57aa9cf3b9f31134df9e8d97a1
Shlomi Haver <shlomi.haver@booking.com>
2021-07-18 21:52:13 +0300

    more comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 import static javax.swing.JOptionPane.showMessageDialog;
 
+// This is an example file for testing
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Program started");
--- TheBestCompiler.java
+++ TheBestCompiler.java
@@ -2,15 +2,16 @@
 
 import java.io.IOException;
 
-
+// This is the best compiler in the world
 public class TheBestCompiler {
     public static void main(String[] args) throws IOException {
-
+        //Validates args
         if (args.length < 1) {
             System.out.println("File wasn't provided for compilation.");
         }
         String filePath = args[0];
 
+        //Compile the code
         try {
             if (filePath != null) {
                 Runtime rt = Runtime.getRuntime();
