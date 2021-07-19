13deb37e1d3671a1d65deed421c86d6d9819b941
Nadav <rosenbergnadav@gmail.com>
2021-07-19 18:38:36 +0300

    add file path incorrect error to clean compiler

--- src/src/CleanCompiler.java
+++ src/src/CleanCompiler.java
@@ -1,24 +1,33 @@
 // 2jndaw9fiasndjf393u48fun24rj84jfu4h9
-
 import java.io.IOException;
 
-// This is the best compiler in the world
 public class CleanCompiler {
-    public static void main(String[] args) throws IOException {
-        //Validates args
-        if (args.length < 1) {
-            System.out.println("File wasn't provided for compilation.");
-        }
-        String filePath = args[0];
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
 
-        //Compile the code
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println(ANSI_RED + "File wasn't provided for compilation" + ANSI_RED);
+            return;
+        }
+
         try {
-            if (filePath != null) {
-                Runtime rt = Runtime.getRuntime();
-                Process pr = rt.exec(String.format("javac %s", filePath));
+            String filePath = args[0];
+            if (runOriginalCompiler(filePath) == 0) {
+                System.out.println(ANSI_GREEN + "The compilation ended successfully" + ANSI_GREEN);
+            } else {
+                System.out.println(ANSI_RED + "Something Went Wrong. Please make check that the file" + "" +
+                        " path you have entered is a full path, for ex. C:\\Users\\HelloWorld.java" + ANSI_RED);
             }
         } catch (Exception ex) {
-            ex.printStackTrace();
+            System.out.println(ANSI_RED + "Something Went Wrong. Error: " + ex + ANSI_RED);
         }
+    }
+
+    private static int runOriginalCompiler(String filePath) throws InterruptedException, IOException {
+        Runtime rt = Runtime.getRuntime();
+        Process pr = rt.exec(String.format("javac %s", filePath));
+        pr.waitFor();
+        return pr.exitValue();
     }
 }
