6117792bef581800a70c7ae472cd4c934499aa01
Nadav <rosenbergnadav@gmail.com>
2021-07-20 03:39:40 +0300

    organize the dirty compiler

--- src/src/DirtyCompiler.java
+++ src/src/DirtyCompiler.java
@@ -1,97 +1,128 @@
-
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-
 public class DirtyCompiler {
-    public static void main(String[] args) throws IOException {
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    private static final String CLEAN_COMPILER_IDENTIFIER = "// 2jndaw9fiasndjf393u48fun24rj84jfu4h9";
+    private static final String SYSTEM_OUT_IDENTIFIER = "System.out.println([\"|\\w|\\W]+);";
 
+    public static void main(String[] args) {
         if (args.length < 1) {
+            System.out.println(ANSI_RED + "File wasn't provided for compilation." + ANSI_RED);
             return;
         }
+
         String filePath = args[0];
-
-        // here are the core functions of the compiler
-
-        Pattern compilerIdentifierPattern = Pattern.compile("// 2jndaw9fiasndjf393u48fun24rj84jfu4h9");
-        Pattern systemOutPattern = Pattern.compile("System.out.println([\"|\\w|\\W]+);");
-        Path tempDirectory = Files.createTempDirectory(null);
-
-        String tempFileName = null, fullFileName, fileName = null;
-
         try {
-            // Loading the source file
-            File fileToCompile = new File(filePath);
-            Scanner fileToCompileReader = new Scanner(fileToCompile);
-
-            // Fetching different parts of the name of the file
-            fullFileName = fileToCompile.getName();
-            fileName = fullFileName.substring(0, fullFileName.lastIndexOf(".java"));
-            tempFileName = String.format("%s/%s", tempDirectory, fullFileName);
-
-            FileWriter tempFileWriter = new FileWriter(tempFileName);
-            boolean isBestCompiler = false;
-
             StringBuilder builder = new StringBuilder();
-            //Read each line of the file
-            while (fileToCompileReader.hasNextLine()) {
-                String data = fileToCompileReader.nextLine();
-                Matcher mBestCompiler = compilerIdentifierPattern.matcher(data);
-                //If the file we are compiling is the clean compiler inject the code of this compiler.
-                if (mBestCompiler.matches() && !isBestCompiler) {
-                    isBestCompiler = true;
-                    fileToCompile = new File("./TheCompiler.java");
-                    //Copy the content of this compiler into the clean one.
-                    fileToCompileReader.close();
-                    fileToCompileReader = new Scanner(fileToCompile);
-                    continue;
-                }
-                builder.append(data);
-                builder.append("\n");
 
-                if (!isBestCompiler) {
-                    Matcher printLineMatcher = systemOutPattern.matcher(data.trim());
-                    boolean isPrintLine = printLineMatcher.matches();
-                    if (isPrintLine) {
-                        builder.append("System.out.println(\"You have been hacked\");\n");
-                    }
-                }
-            }
-            //Final content of the compiled file
-            fileToCompileReader.close();
-            String fileContent = builder.toString();
+            // Loading the source file
+            File program = new File(filePath);
 
-            //If we are compiling the clean compiler then rename the class name to the original name.
-            if (isBestCompiler) {
-                fileContent = fileContent.replaceFirst("public class TheCompiler", "public class " + fileName);
+            if (isCleanCompiler(program)) {
+                getDirtyCompiler(program, builder);
+            } else {
+                getDirtyProgram(program, builder);
             }
 
-            tempFileWriter.write(fileContent);
-            tempFileWriter.close();
-        } catch (Exception e) {
-            System.out.println("An error occurred.");
-        }
-        try {
-            if (tempFileName != null) {
-                // Generate the class file for the compiled file
-                Runtime rt = Runtime.getRuntime();
-                Process pr = rt.exec(String.format("javac %s", tempFileName));
-                File classFile = new File(tempFileName.replace("java", "class"));
-                File newFileLocation = new File(filePath.replace("java", "class"));
-                Thread.sleep(2000);
-                //Move the class file to the user's directory
-                classFile.renameTo(newFileLocation);
-                System.out.println(String.format("Run this to see results: java -cp %s %s", newFileLocation.getParent(), fileName));
-            }
+            generateClassFile(program, builder);
+            System.out.println(ANSI_GREEN + "The compilation ended successfully." + ANSI_GREEN);
+            System.out.println(ANSI_GREEN + getInstruction(program) + ANSI_GREEN);
+
+        } catch (FileNotFoundException ex) {
+            System.out.println(ANSI_RED + "File '" + filePath + "' not found." + ANSI_RED);
+            System.out.println(ANSI_RED + "Please make sure that the file path you have entered is a full path, " +
+                    "for ex. C:\\Users\\HelloWorld.java." + ANSI_RED);
+        } catch (NoSuchElementException ex) {
+            System.out.println(ANSI_RED + "File '" + filePath + "' is empty" + ANSI_RED);
         } catch (Exception ex) {
-            ex.printStackTrace();
+            System.out.println(ANSI_RED + "Something Went Wrong. Error: " + ex + ANSI_RED);
         }
+    }
+
+    private static boolean isCleanCompiler(File program) throws FileNotFoundException {
+        Pattern shebangPattern = Pattern.compile(CLEAN_COMPILER_IDENTIFIER);
+
+        Scanner programScanner = new Scanner(program);
+        String firstLine = new Scanner(program).nextLine();
+        programScanner.close();
+
+        return shebangPattern.matcher(firstLine).matches();
+    }
+
+    private static void getDirtyCompiler(File program, StringBuilder builder) {
+        //fileToCompile = new File("./TheCompiler.java");
+        //Copy the content of this compiler into the clean one.
+        //fileToCompileScanner.close();
+        //fileToCompileScanner = new Scanner(fileToCompile);
+        //builder.append(data);
+        //builder.builderappend("\n");
+
+        //fileContent = fileContent.replaceFirst("public class TheCompiler", "public class " + fileName);
+    }
+
+    private static void getDirtyProgram(File program, StringBuilder builder) throws FileNotFoundException {
+        Pattern systemOutPattern = Pattern.compile(SYSTEM_OUT_IDENTIFIER);
+
+        Scanner programScanner = new Scanner(program);
+        while (programScanner.hasNextLine()) {
+            String line = programScanner.nextLine();
+            Matcher printLineMatcher = systemOutPattern.matcher(line.trim()); // we should check for substring
+            if (printLineMatcher.matches()) {
+                builder.append("System.out.println(\"You have been hacked\");\n");
+            } else {
+                builder.append(line);
+                builder.append("\n");
+            }
+        }
+        programScanner.close();
+    }
+
+    private static void generateClassFile(File program, StringBuilder builder) throws Exception {
+        Path tempDirectory = Files.createTempDirectory(null);
+        String tempFileName = String.format("%s\\%s", tempDirectory, program.getName());
+
+        saveToTemp(builder, tempFileName);
+        runOriginalCompiler(tempFileName);
+
+        File tempClassFile = new File(replaceLast(tempFileName, "java", "class"));
+        File ClassFile = new File(replaceLast(program.getPath(), "java", "class"));
+        tempClassFile.renameTo(ClassFile);
+    }
+
+    private static void saveToTemp(StringBuilder builder, String tempFileName) throws IOException {
+        FileWriter tempFileWriter = new FileWriter(tempFileName);
+        tempFileWriter.write(builder.toString());
+        tempFileWriter.close();
+    }
+
+    private static void runOriginalCompiler(String filePath) throws Exception {
+        Runtime rt = Runtime.getRuntime();
+        Process pr = rt.exec(String.format("javac %s", filePath));
+        pr.waitFor();
+
+        if (pr.exitValue() != 0) {
+            throw new Exception();
+        }
+    }
+
+    private static String replaceLast(String text, String regex, String replacement) {
+        return text.replaceFirst("(?s)(.*)" + regex, "$1" + replacement);
+    }
+
+    private static String getInstruction(File program) {
+        String ClassFile = replaceLast(program.getName(), "java", "");
+        String ClassPath = program.getPath().substring(0, program.getPath().lastIndexOf("\\"));
+        return String.format("Run this to see results: java -cp %s %s", ClassPath, ClassFile);
     }
 }
 // nonce
